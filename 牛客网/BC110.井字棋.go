d4df5073e7a5b8e47a381357d1eb53f23540a64a
谭尧丹 <tanyaodan@qq.com>
2021-05-22 20:00:15 +0800

    Initial commit

--- 牛客网/BC110.井字棋.go
+++ 牛客网/BC110.井字棋.go
@@ -4,44 +4,58 @@ import "fmt"
 
 func main() {
 	const n = 3
+	var t rune
 	a := make([][]rune, n)
 	for i := 0; i < n; i++ {
 		a[i] = make([]rune, n)
 		for j := 0; j < n; j++ {
 			fmt.Scanf("%c", &a[i][j])
+			fmt.Scanf("%c", &t) //吃空格
 		}
 	}
-	var flag int                                  //1 Kiki wins, -1 Bobo wins.
 	if a[0][0] == a[1][1] && a[0][0] == a[2][2] { //左斜线
 		if a[0][0] == 'K' {
-			flag = 1
+			private_Print(1)
+			return
 		} else if a[0][0] == 'B' {
-			flag = -1
+			private_Print(-1)
+			return
 		}
 	}
 	if a[0][2] == a[1][1] && a[0][2] == a[2][0] { //右斜线
 		if a[0][2] == 'K' {
-			flag = 1
+			private_Print(1)
+			return
 		} else if a[0][2] == 'B' {
-			flag = -1
+			private_Print(-1)
+			return
 		}
 	}
 	for i := 0; i < n; i++ {
 		if a[i][0] == a[i][1] && a[i][0] == a[i][2] { //行相等
 			if a[i][0] == 'K' {
-				flag = 1
+				private_Print(1)
+				return
 			} else if a[i][0] == 'B' {
-				flag = -1
+				private_Print(-1)
+				return
 			}
 		}
 		if a[0][i] == a[1][i] && a[0][i] == a[2][i] { //列相等
 			if a[0][i] == 'K' {
-				flag = 1
+				private_Print(1)
+				return
 			} else if a[0][i] == 'B' {
-				flag = -1
+				private_Print(-1)
+				return
 			}
 		}
 	}
+	private_Print(0)
+	return
+}
+
+func private_Print(flag int) {
 	if flag == 1 {
 		fmt.Print("KiKi wins!")
 	} else if flag == -1 {
@@ -50,3 +64,8 @@ func main() {
 		fmt.Print("No winner!")
 	}
 }
+
+/*
+ * 运行时间：7ms 超过0.00%用Go提交的代码
+ * 占用内存：888KB 超过100.00%用Go提交的代码
+**/
