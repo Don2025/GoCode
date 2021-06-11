96488d854ba7cc9b2a853e800063af9cfa8bbaa9
谭尧丹 <tanyaodan@qq.com>
2021-06-11 17:08:25 +0800

    Initial commit

---
+++ LeetCode/279.完全平方数.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+	}
+	for i := 2; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		n, _ := strconv.Atoi(input.Text())
+		println(numSquares(n))
+	}
+}
+
+/*
+ * 执行用时：40ms 在所有Go提交中击败了59.73%的用户
+ * 占用内存：6.2MB 在所有Go提交中击败了59.28%的用户
+**/
