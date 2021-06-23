0366c78fc7c2ff4c0c93d8da6850250920b24ae9
谭尧丹 <tanyaodan@qq.com>
2021-06-23 09:12:42 +0800

    Initial commit

---
+++ LeetCode/165.比较版本号.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func compareVersion(version1 string, version2 string) int {
+	arr1, arr2 := strings.Split(version1, "."), strings.Split(version2, ".")
+	for i := 0; i < max(len(arr1), len(arr2)); i++ {
+		n1, n2 := getValue(arr1, i), getValue(arr2, i)
+		if n1 < n2 {
+			return -1
+		} else if n1 > n2 {
+			return 1
+		}
+	}
+	return 0
+}
+
+func getValue(arr []string, n int) int {
+	if n < len(arr) {
+		ans, _ := strconv.Atoi(arr[n])
+		return ans
+	}
+	return 0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		version1 := input.Text()
+		input.Scan()
+		version2 := input.Text()
+		println(compareVersion(version1, version2))
+	}
+}
