87d6e4e20a3c23268321f168c8dc6a2213c8879c
谭尧丹 <tanyaodan@qq.com>
2021-09-27 10:53:33 +0800

    Initial commit

---
+++ LeetCode/639.解码方法II.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func numDecodings(s string) int {
+	const mod int = 1e9 + 7
+	a, b, c := 0, 1, 0
+	for i := range s {
+		c = b * check1digit(s[i]) % mod
+		if i > 0 {
+			c = (c + a*check2digits(s[i-1], s[i])) % mod
+		}
+		a, b = b, c
+	}
+	return c
+}
+
+func check1digit(ch byte) int {
+	if ch == '*' {
+		return 9
+	}
+	if ch == '0' {
+		return 0
+	}
+	return 1
+}
+
+func check2digits(c0, c1 byte) int {
+	if c0 == '*' && c1 == '*' {
+		return 15
+	}
+	if c0 == '*' {
+		if c1 <= '6' {
+			return 2
+		}
+		return 1
+	}
+	if c1 == '*' {
+		if c0 == '1' {
+			return 9
+		}
+		if c0 == '2' {
+			return 6
+		}
+		return 0
+	}
+	if c0 != '0' && (c0-'0')*10+(c1-'0') <= 26 {
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		println(numDecodings(input.Text()))
+	}
+}
+
+/*
+ * 执行用时：20ms 在所有Go提交中击败了10.00%的用户
+ * 占用内存：6.2MB 在所有Go提交中击败了100.00%的用户
+**/
