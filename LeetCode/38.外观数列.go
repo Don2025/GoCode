0696fadd2b04bccef7b7198c87f61313b98165a3
谭尧丹 <tanyaodan@qq.com>
2021-10-15 12:55:34 +0800

    Initial commit

---
+++ LeetCode/38.外观数列.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func countAndSay(n int) string {
+	prev := "1"
+	for i := 2; i <= n; i++ {
+		cur := &strings.Builder{}
+		for j, start := 0, 0; j < len(prev); start = j {
+			for j < len(prev) && prev[j] == prev[start] {
+				j++
+			}
+			cur.WriteString(strconv.Itoa(j - start))
+			cur.WriteByte(prev[start])
+		}
+		prev = cur.String()
+	}
+	return prev
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		n, _ := strconv.Atoi(input.Text())
+		println(countAndSay(n))
+	}
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.4MB 在所有Go提交中击败了89.90%的用户
+**/
