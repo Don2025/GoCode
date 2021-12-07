7ec62ba3bcfe3223c9d3e6a57eb499dad6230cf1
Don2025 <tanyaodan@qq.com>
2021-12-07 09:29:31 +0800

    Initial commit

--- 牛客网/BC76.公务员面试.go
+++ 牛客网/BC76.公务员面试.go
@@ -6,22 +6,24 @@ import (
 )
 
 func main() {
-	var a [7]int
-	for i := 0; i < 7; i++ {
-		_, err := fmt.Scan(&a[i])
-		if err != nil {
-			return
+	for {
+		a := make([]int, 7)
+		for i := 0; i < 7; i++ {
+			_, err := fmt.Scan(&a[i])
+			if err != nil {
+				return
+			}
 		}
+		sort.Ints(a)
+		var sum int
+		for i := 1; i < 6; i++ {
+			sum += a[i]
+		}
+		fmt.Printf("%.2f\n", float64(sum)/5)
 	}
-	sort.Ints(a[:])
-	sum := 0
-	for i := 1; i < 6; i++ {
-		sum += a[i]
-	}
-	fmt.Printf("%.2f", float64(sum)/5)
 }
 
 /*
- * 运行时间：9ms 超过22.22%用Go提交的代码
- * 占用内存：1080KB 超过0.00%用Go提交的代码
+ * 运行时间：3ms 超过50.00%用Go提交的代码
+ * 占用内存：1088KB 超过0.00%用Go提交的代码
 **/
