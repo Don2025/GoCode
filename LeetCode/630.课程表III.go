3076f9c3070b67623111eda1dfcdecd6c2deb049
Don2025 <tanyaodan@qq.com>
2021-12-18 09:38:02 +0800

    Initial commit

---
+++ LeetCode/630.课程表III.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func scheduleCourse(courses [][]int) int {
+	sort.Slice(courses, func(i, j int) bool {
+		return courses[i][1] < courses[j][1]
+	})
+
+	h := &Heap{}
+	total := 0 // 优先队列中所有课程的总时间
+	for _, course := range courses {
+		if t := course[0]; total+t <= course[1] {
+			total += t
+			heap.Push(h, t)
+		} else if h.Len() > 0 && t < h.IntSlice[0] {
+			total += t - h.IntSlice[0]
+			h.IntSlice[0] = t
+			heap.Fix(h, 0)
+		}
+	}
+	return h.Len()
+}
+
+type Heap struct {
+	sort.IntSlice
+}
+
+func (h Heap) Less(i, j int) bool {
+	return h.IntSlice[i] > h.IntSlice[j]
+}
+
+func (h *Heap) Push(x interface{}) {
+	h.IntSlice = append(h.IntSlice, x.(int))
+}
+
+func (h *Heap) Pop() interface{} {
+	a := h.IntSlice
+	x := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return x
+}
+
+/*
+ * 执行用时：128ms 在所有Go提交中击败了21.86%的用户
+ * 占用内存：7.3MB 在所有Go提交中击败了53.77%的用户
+**/
