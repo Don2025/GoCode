3587a7d167766ef6220f437b7e4ec3dbe1c7baf2
Don2025 <tanyaodan@qq.com>
2021-12-22 09:35:05 +0800

    Initial commit

---
+++ LeetCode/876.链表的中间结点.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		head := intArrayToLinkList(stringArrayToIntArray(strings.Fields(input.Text())))
+		printLinkedList(middleNode(head))
+	}
+}
+
+func intArrayToLinkList(arr []int) *ListNode {
+	var head *ListNode = new(ListNode)
+	p := head
+	for _, x := range arr {
+		p.Next = &ListNode{Val: x}
+		p = p.Next
+	}
+	return head.Next
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+//带值头结点
+func printLinkedList(head *ListNode) {
+	for p := head; p != nil; p = p.Next {
+		if p != head {
+			fmt.Printf("->")
+		}
+		fmt.Printf("%d", p.Val)
+	}
+	println()
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2MB 在所有Go提交中击败了100.00%的用户
+**/
