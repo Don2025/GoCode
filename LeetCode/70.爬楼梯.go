822bb28dac4ef32aee87d812523cd0f9bc41eabc
Don2025 <tanyaodan@qq.com>
2021-12-29 09:08:21 +0800

    Initial commit

---
+++ LeetCode/70.爬楼梯.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func climbStairs(n int) int {
+	if n == 1 || n == 2 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[1], dp[2] = 1, 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		n, _ := strconv.Atoi(input.Text())
+		println(climbStairs(n))
+	}
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：1.9MB 在所有Go提交中击败了33.96%的用户
+**/
