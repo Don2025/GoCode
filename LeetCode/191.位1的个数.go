9549f71faab2fd991db6c9d489bdfcefcc628caa
Don2025 <tanyaodan@qq.com>
2021-12-29 14:48:49 +0800

    Initial commit

---
+++ LeetCode/191.位1的个数.go
@@ -0,0 +1,15 @@
+package main
+
+func hammingWeight(num uint32) int {
+	cnt := 0
+	for num != 0 {
+		num &= (num - 1)
+		cnt++
+	}
+	return cnt
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：1.9MB 在所有Go提交中击败了100.00%的用户
+**/
