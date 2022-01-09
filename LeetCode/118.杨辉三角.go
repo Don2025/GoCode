568c05677658854157a98ca7334bd1c9fc8fadef
谭尧丹 <tanyaodan@qq.com>
2022-01-09 10:20:24 +0800

    Initial commit

---
+++ LeetCode/118.杨辉三角.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+	for i := range ans {
+		ans[i] = make([]int, i+1)
+		ans[i][0], ans[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+	}
+	return ans
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		n, _ := strconv.Atoi(input.Text())
+		ans := generate(n)
+		for _, row := range ans {
+			fmt.Printf("%v ", row)
+		}
+		fmt.Println()
+	}
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2MB 在所有Go提交中击败了50.25%的用户
+**/
