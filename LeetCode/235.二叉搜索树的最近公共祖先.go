e520c203f77f5aac6e21f08c14bfc2df020b6625
谭尧丹 <tanyaodan@qq.com>
2022-01-15 11:15:50 +0800

    Initial commit

---
+++ LeetCode/235.二叉搜索树的最近公共祖先.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
+
+/*
+ * 执行用时：20ms 在所有Go提交中击败了66.16%的用户
+ * 占用内存：6.9MB 在所有Go提交中击败了28.34%的用户
+**/
