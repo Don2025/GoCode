1d017124756e7ef85df4e4e1d0981082c4d4fd21
Don2025 <tanyaodan@qq.com>
2022-02-21 07:53:27 +0800

    Initial commit

---
+++ LeetCode/334.递增的三元子序列.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func increasingTriplet(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	first, second := nums[0], math.MaxInt32
+	for i := 1; i < n; i++ {
+		num := nums[i]
+		if num > second {
+			return true
+		} else if num > first {
+			second = num
+		} else {
+			first = num
+		}
+	}
+	return false
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		nums := stringArrayToIntArray(strings.Fields(input.Text()))
+		println(increasingTriplet(nums))
+	}
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+/*
+ * 执行用时：56ms 在所有Go提交中击败了94.63%的用户
+ * 占用内存：17MB 在所有Go提交中击败了97.99%的用户
+**/
