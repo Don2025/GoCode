43b4d2cd329c00e6b0fd91b885274d4c0901a887
Don2025 <tanyaodan@qq.com>
2022-02-23 07:05:54 +0800

    Initial commit

---
+++ LeetCode/2.两数相加.go
@@ -0,0 +1,40 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var tail, head *ListNode
+	var carry int
+	for l1 != nil || l2 != nil {
+		n1, n2 := 0, 0
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := n1 + n2 + carry
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return head
+}
+
+/*
+ * 执行用时：12ms 在所有Go提交中击败了48.74%的用户
+ * 占用内存：4.4MB 在所有Go提交中击败了72.79%的用户
+**/
