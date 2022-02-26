0b7928c4e2273ed99beb2ec2cb9761933f9e5e27
Don2025 <tanyaodan@qq.com>
2022-02-26 09:51:10 +0800

    Initial commit

---
+++ LeetCode/322.零钱兑换.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = amount + 1
+	}
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		coins := stringArrayToIntArray(strings.Fields(input.Text()))
+		input.Scan()
+		amount, _ := strconv.Atoi(input.Text())
+		println(coinChange(coins, amount))
+	}
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+/*
+ * 执行用时：12ms 在所有Go提交中击败了51.97%的用户
+ * 占用内存：6.3MB 在所有Go提交中击败了73.79%的用户
+**/
