959ccaa1879f4a9412ddaf6c2cc5239bc03c07d5
Don2025 <tanyaodan@qq.com>
2022-03-03 08:24:59 +0800

    Initial commit

---
+++ LeetCode/44.通配符匹配.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func isMatch(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+	for i := 1; i <= n; i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = true
+		} else {
+			break
+		}
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-1] || dp[i-1][j]
+			} else if p[j-1] == '?' || s[i-1] == p[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		arr := strings.Fields(input.Text())
+		println(isMatch(arr[0], arr[1]))
+	}
+}
+
+/*
+ * 执行用时：20ms 在所有Go提交中击败了22.28%的用户
+ * 占用内存：6.3MB 在所有Go提交中击败了65.06%的用户
+**/
