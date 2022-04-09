a54b9cf745720cb947732f8ea0162aeb6cdb5911
Don2025 <tanyaodan@qq.com>
2022-04-10 00:41:15 +0800

    Initial commit

---
+++ LeetCode/380.O(1)时间插入、删除和获取随机元素.go
@@ -0,0 +1,51 @@
+package main
+
+import "math/rand"
+
+type RandomizedSet struct {
+	Nums     []int
+	GetIndex map[int]int
+}
+
+func Constructor() RandomizedSet {
+	return RandomizedSet{[]int{}, map[int]int{}}
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+	if _, ok := this.GetIndex[val]; ok {
+		return false
+	}
+	this.Nums = append(this.Nums, val)
+	this.GetIndex[val] = len(this.Nums) - 1
+	return true
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+	if _, ok := this.GetIndex[val]; !ok {
+		return false
+	}
+	index := this.GetIndex[val]
+	lastIndex := len(this.Nums) - 1
+	this.GetIndex[this.Nums[lastIndex]] = index
+	this.Nums[index], this.Nums[lastIndex] = this.Nums[lastIndex], this.Nums[index]
+	this.Nums = this.Nums[0:lastIndex]
+	delete(this.GetIndex, val)
+	return true
+}
+
+func (this *RandomizedSet) GetRandom() int {
+	return this.Nums[rand.Intn(len(this.Nums))]
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
+
+/*
+ * 执行用时：160ms 在所有Go提交中击败了29.12%的用户
+ * 占用内存：43.5MB 在所有Go提交中击败了99.73%的用户
+**/
