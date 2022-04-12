1030358ecff7b1f9d040d7489ccdbe9e1d13ccd5
Don2025 <tanyaodan@qq.com>
2022-04-12 09:24:28 +0800

    Initial commit

---
+++ LeetCode/90.子集II.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
+	var t []int
+	var ans [][]int
+	var dfs func(bool, int)
+	dfs = func(choosePre bool, cur int) {
+		if cur == len(nums) {
+			ans = append(ans, append([]int(nil), t...))
+			return
+		}
+		dfs(false, cur+1)
+		if !choosePre && cur > 0 && nums[cur-1] == nums[cur] {
+			return
+		}
+		t = append(t, nums[cur])
+		dfs(true, cur+1)
+		t = t[:len(t)-1]
+	}
+	dfs(false, 0)
+	return ans
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		nums := stringArrayToIntArray(strings.Fields(input.Text()))
+		ans := subsetsWithDup(nums)
+		for _, x := range ans {
+			fmt.Printf("%v ", x)
+		}
+		fmt.Println()
+	}
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.3MB 在所有Go提交中击败了84.44%的用户
+**/
