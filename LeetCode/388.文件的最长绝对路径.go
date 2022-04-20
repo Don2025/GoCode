5553414d0a60613419573a8c56e8fd300155ac57
Don2025 <tanyaodan@qq.com>
2022-04-20 09:35:32 +0800

    Initial commit

---
+++ LeetCode/388.文件的最长绝对路径.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func lengthLongestPath(input string) int {
+	n := len(input)
+	var ans int
+	level := make([]int, n+1)
+	for i := 0; i < n; {
+		// 检测当前文件的深度
+		depth := 1
+		for ; i < n && input[i] == '\t'; i++ {
+			depth++
+		}
+
+		// 统计当前文件名的长度
+		length, isFile := 0, false
+		for ; i < n && input[i] != '\n'; i++ {
+			if input[i] == '.' {
+				isFile = true
+			}
+			length++
+		}
+		i++ // 跳过换行符
+
+		if depth > 1 {
+			length += level[depth-1] + 1
+		}
+		if isFile {
+			ans = max(ans, length)
+		} else {
+			level[depth] = length
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		println(lengthLongestPath(input.Text()))
+	}
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：1.9MB 在所有Go提交中击败了73.91%的用户
+**/
