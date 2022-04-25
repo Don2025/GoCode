0b07467ca03ddf146c7755b6a6803816e35b88b5
Don2025 <tanyaodan@qq.com>
2022-04-25 09:01:09 +0800

    Initial commit

---
+++ LeetCode/398.随机数索引.go
@@ -0,0 +1,33 @@
+package main
+
+import "math/rand"
+
+type Solution []int
+
+func Constructor(nums []int) Solution {
+	return nums
+}
+
+func (nums Solution) Pick(target int) int {
+	var cnt, ans int
+	for i, num := range nums {
+		if num == target {
+			cnt++
+			if rand.Intn(cnt) == 0 {
+				ans = i
+			}
+		}
+	}
+	return ans
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Pick(target);
+ */
+
+/*
+ * 执行用时：68ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：8MB 在所有Go提交中击败了97.74%的用户
+**/
