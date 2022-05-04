011774dde7d24ee9ded6d86504553b9867ecfcdf
谭尧丹 <tanyaodan@qq.com>
2022-05-05 07:35:45 +0800

    Initial commit

---
+++ LeetCode/933.最近的请求次数.go
@@ -0,0 +1,26 @@
+package main
+
+type RecentCounter []int
+
+func Constructor() (r RecentCounter) {
+	return
+}
+
+func (r *RecentCounter) Ping(t int) int {
+	*r = append(*r, t)
+	for (*r)[0] < t-3000 {
+		*r = (*r)[1:]
+	}
+	return len(*r)
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
+
+/*
+ * 执行用时：100ms 在所有Go提交中击败了81.21%的用户
+ * 占用内存：8.1MB 在所有Go提交中击败了42.28%的用户
+**/
