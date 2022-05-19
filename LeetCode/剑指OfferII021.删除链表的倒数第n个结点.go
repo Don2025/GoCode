c68b6f2d1cbd72dc7896d96dda6cb99bb3661fd3
Don2025 <tanyaodan@qq.com>
2022-05-20 06:56:23 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII021.删除链表的倒数第n个结点.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		head := intArrayToLinkList(stringArrayToIntArray(strings.Fields(input.Text())))
+		input.Scan()
+		n, _ := strconv.Atoi(input.Text())
+		ans := removeNthFromEnd(head, n)
+		printLinkedList(ans)
+	}
+}
+
+func intArrayToLinkList(arr []int) *ListNode {
+	var head *ListNode = new(ListNode)
+	p := head
+	for _, x := range arr {
+		p.Next = &ListNode{Val: x}
+		p = p.Next
+	}
+	return head.Next
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+//带值头结点
+func printLinkedList(head *ListNode) {
+	for p := head; p != nil; p = p.Next {
+		if p != head {
+			fmt.Printf("->")
+		}
+		fmt.Printf("%d", p.Val)
+	}
+	println()
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.1MB 在所有Go提交中击败了100.00%的用户
+**/
