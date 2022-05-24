4bee51c331974db94d8d04a7fd93ee5e16efa799
Don2025 <tanyaodan@qq.com>
2022-05-24 10:23:01 +0800

    Initial commit

---
+++ LeetCode/剑指 OfferII029.排序的循环链表.go
@@ -0,0 +1,32 @@
+package main
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func insert(head *Node, x int) *Node {
+	if head == nil {
+		head = &Node{x, nil}
+		head.Next = head
+		return head
+	}
+	cur := head
+	for cur.Next != head {
+		if cur.Next.Val < cur.Val {
+			if x >= cur.Val || x <= cur.Next.Val {
+				break
+			}
+		} else if x >= cur.Val && x <= cur.Next.Val {
+			break
+		}
+		cur = cur.Next
+	}
+	cur.Next = &Node{x, cur.Next}
+	return head
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：3.5MB 在所有Go提交中击败了74.52%的用户
+**/
