a39ecb66cca133b76fcef37fd61013ab5bc07714
Don2025 <tanyaodan@qq.com>
2022-05-31 09:26:30 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII114.外星文字典.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func alienOrder(words []string) string {
+	g := make(map[byte][]byte)
+	for _, x := range words[0] {
+		g[byte(x)] = nil
+	}
+next:
+	for i := 1; i < len(words); i++ {
+		s, t := words[i-1], words[i]
+		for _, x := range t {
+			g[byte(x)] = g[byte(x)]
+		}
+		for j := 0; j < len(s) && j < len(t); j++ {
+			if s[j] != t[j] {
+				g[s[j]] = append(g[s[j]], t[j])
+				continue next
+			}
+		}
+		if len(s) > len(t) {
+			return ""
+		}
+	}
+	const visiting, visited = 1, 2
+	order := make([]byte, len(g))
+	i := len(g) - 1
+	state := make(map[byte]int)
+	var dfs func(u byte) bool
+	dfs = func(u byte) bool {
+		state[u] = visiting
+		for _, v := range g[u] {
+			if state[v] == 0 {
+				if !dfs(v) {
+					return false
+				}
+			} else if state[v] == visiting {
+				return false
+			}
+		}
+		order[i] = u
+		i--
+		state[u] = visited
+		return true
+	}
+	for u := range g {
+		if state[u] == 0 && !dfs(u) {
+			return ""
+		}
+	}
+	return string(order)
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		words := strings.Fields(input.Text())
+		println(alienOrder(words))
+	}
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.2MB 在所有Go提交中击败了34.61%的用户
+**/
