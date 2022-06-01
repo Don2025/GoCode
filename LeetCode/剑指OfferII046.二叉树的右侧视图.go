74bb5f57014345867e2f49ba647a935bb362cc13
Don2025 <tanyaodan@qq.com>
2022-06-01 20:04:33 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII046.二叉树的右侧视图.go
@@ -0,0 +1,31 @@
+package main
+
+func rightSideView(root *TreeNode) []int {
+	var ans []int
+	if root == nil {
+		return ans
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i == size-1 {
+				ans = append(ans, node.Val)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return ans
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.1MB 在所有Go提交中击败了82.96%的用户
+**/
