83a9e0f8f218893d38b461144cc48ec97ed726d3
Don2025 <tanyaodan@qq.com>
2022-06-02 10:13:25 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII041.滑动窗口的平均值.go
@@ -0,0 +1,33 @@
+package main
+
+type MovingAverage struct {
+	Queue []int
+	Size  int
+	Sum   float64
+}
+
+/** Initialize your data structure here. */
+func Constructor(size int) MovingAverage {
+	return MovingAverage{[]int{}, size, 0}
+}
+
+func (ma *MovingAverage) Next(val int) float64 {
+	if len(ma.Queue) == ma.Size {
+		ma.Sum -= float64(ma.Queue[0])
+		ma.Queue = ma.Queue[1:]
+	}
+	ma.Queue = append(ma.Queue, val)
+	ma.Sum += float64(val)
+	return ma.Sum / float64(len(ma.Queue))
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * obj := Constructor(size);
+ * param_1 := obj.Next(val);
+ */
+
+/*
+ * 执行用时：12ms 在所有Go提交中击败了95.92%的用户
+ * 占用内存：7.4MB 在所有Go提交中击败了49.66%的用户
+**/
