06240a6c1f5216ae662b54809f99b0351d75be4c
谭尧丹 <tanyaodan@qq.com>
2022-06-07 21:22:05 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII050.向下的路径节点之和.go
@@ -0,0 +1,26 @@
+package main
+
+func pathSum(root *TreeNode, targetSum int) int {
+	var ans int
+	preSum := map[int]int{0: 1}
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, cur int) {
+		if node == nil {
+			return
+		}
+		cur += node.Val
+		ans += preSum[cur-targetSum]
+		preSum[cur]++
+		dfs(node.Left, cur)
+		dfs(node.Right, cur)
+		preSum[cur]--
+		return
+	}
+	dfs(root, 0)
+	return ans
+}
+
+/*
+ * 执行用时：4ms 在所有Go提交中击败了94.57%的用户
+ * 占用内存：4.8MB 在所有Go提交中击败了31.01%的用户
+**/
