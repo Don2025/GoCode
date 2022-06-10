63489ffe051503e26168ec4b267d475543243a14
Don2025 <tanyaodan@qq.com>
2022-06-10 13:04:25 +0800

    Initial commit

---
+++ LeetCode/剑指OfferII104.排列的数目.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 1; i <= target; i++ {
+		for _, x := range nums {
+			if x <= i {
+				dp[i] += dp[i-x]
+			}
+		}
+	}
+	return dp[target]
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		nums := stringArrayToIntArray(strings.Fields(input.Text()))
+		input.Scan()
+		target, _ := strconv.Atoi(input.Text())
+		println(combinationSum4(nums, target))
+	}
+}
+
+func stringArrayToIntArray(strings []string) []int {
+	var arr []int
+	for _, x := range strings {
+		n, _ := strconv.Atoi(x)
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+/*
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：1.8MB 在所有Go提交中击败了52.43%的用户
+**/
