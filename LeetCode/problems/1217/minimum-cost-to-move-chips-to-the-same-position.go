767d73388ef61ad34211f6094cdb8551508cadd3
Don2025 <tanyaodan@qq.com>
2022-07-11 16:11:14 +0800

    Update

--- LeetCode/problems/1217/minimum-cost-to-move-chips-to-the-same-position.go
+++ LeetCode/problems/1217/minimum-cost-to-move-chips-to-the-same-position.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+// https://leetcode.cn/problems/minimum-cost-to-move-chips-to-the-same-position/
+//------------------------Leetcode Problem 1217------------------------
 func minCostToMoveChips(position []int) int {
 	var odd, even int
 	for i := 0; i < len(position); i++ {
@@ -25,6 +27,13 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+//------------------------Leetcode Problem 1217------------------------
+/*
+ * https://leetcode.cn/problems/minimum-cost-to-move-chips-to-the-same-position/
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2MB 在所有Go提交中击败了100.00%的用户
+**/
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
