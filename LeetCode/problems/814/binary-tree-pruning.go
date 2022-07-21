1e3c546b2a0676cdbc3137d00431739cf3b4d572
Don2025 <tanyaodan@qq.com>
2022-07-21 19:27:17 +0800

    Update

---
+++ LeetCode/problems/814/binary-tree-pruning.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"github.com/Don2025/GoCode/structures"
+	"github.com/Don2025/GoCode/utils"
+	"os"
+)
+
+type TreeNode = structures.TreeNode
+
+// https://leetcode.cn/problems/binary-tree-pruning/
+//------------------------Leetcode Problem 814------------------------
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+	if root.Left == nil && root.Right == nil && root.Val == 0 {
+		return nil
+	}
+	return root
+}
+
+//------------------------Leetcode Problem 814------------------------
+/*
+ * https://leetcode.cn/problems/binary-tree-pruning/
+ * 执行用时：0ms 在所有Go提交中击败了100.00%的用户
+ * 占用内存：2.2MB 在所有Go提交中击败了100.00%的用户
+**/
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		root := utils.StringToTreeNode(scanner.Text())
+		Printf("Output: %v\n", structures.TreeToLevelOrderStrings(pruneTree(root)))
+	}
+}
